bea888cda1ec3768ebe4a134d4e14cf2abb10e1b
SilverAnton <cu4hawk@gmail.com>
2024-02-23 22:33:01 +0700

    fix main.py

--- src/main.py
+++ src/main.py
@@ -1,31 +1,18 @@
 from src.category import Category
 from src.product import Product
-from src.utils import get_object_product, get_object_category
+from src.utils import get_category, get_product
 
 
 def main_foo(paste_class):
-    """Главная функция печатает экземпляры классов и их атрибуты, на выбор из классов Product и Category"""
-    if paste_class is Category:
-        category = get_object_category('products.json')
-        object_category = Category(category['name'], category['description'], category['products'])
-        print(object_category.name)
-        print(object_category.description)
-        print(object_category.products)
-        print(object_category.category_count)
-        print(object_category.unique_products_count)
-    elif paste_class is Product:
-        product = get_object_product()
-        object_product = Product(product['name'], product['description'], product['price'], product['quantity'])
-        print(object_product.name)
-        print(object_product.description)
-        print(object_product.price)
-        print(object_product.quantity_in_stock)
-    else:
-        print('...ой...')
+    """Главная функция возвращает объект класса, в зависимости от выбранного класса в качестве аргумента"""
+    object_product = Product(get_product()['name'], get_product()['description'], get_product()['price'],
+                             get_product()['quantity'])
+    object_category = Category(get_category()['name'], get_category()['description'], get_category()['products'])
+    if paste_class == Product:
+        return object_product
+    elif paste_class == Category:
+        return object_category
 
 
-main_foo(Category)
-print()
-main_foo(Product)
-print()
-main_foo(get_object_product())
+print(main_foo(Category).products)
+print(main_foo(Product).description)
