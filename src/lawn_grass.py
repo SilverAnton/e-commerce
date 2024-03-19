a12aecc82b154e22b931c3939942240a78579e6c
SilverAnton <cu4hawk@gmail.com>
2024-03-19 20:05:06 +0700

    update code(add parent-class MixinCreateObject)

--- src/lawn_grass.py
+++ src/lawn_grass.py
@@ -1,10 +1,12 @@
 from src.product import Product
-class Lawn_grass(Product):
-    made_in:str
-    germ_period:str
-    color:str
 
-    def __init__(self,name, description, price, quantity_in_stock, made_in, germ_period, color):
+
+class LawnGrass(Product):
+    made_in: str
+    germ_period: str
+    color: str
+
+    def __init__(self, name, description, price, quantity_in_stock, made_in, germ_period, color):
         """Инициализатор названия объекта, описания объекта, цены, количества товара в наличии, страна производитель,
          период прорастания, цвет"""
         self.made_in = made_in
