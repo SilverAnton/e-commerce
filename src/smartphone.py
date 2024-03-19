a693643caf17cd23117a963f77740ec68b72113a
SilverAnton <cu4hawk@gmail.com>
2024-03-19 20:05:27 +0700

    update code(add parent-class MixinCreateObject)

--- src/smartphone.py
+++ src/smartphone.py
@@ -1,22 +1,17 @@
 from src.product import Product
 
-class Smartphone(Product):
-    perfomance:float
-    model:str
-    memory:float
-    color:str
 
-    def __init__(self,name, description, price, quantity_in_stock, perfomance, model, memory, color):
-        """Инициализатор названия объекта, описания объекта, цены, количества товара в наличии, производитнльность
+class Smartphone(Product):
+    performance: float
+    model: str
+    memory: float
+    color: str
+
+    def __init__(self, name, description, price, quantity_in_stock, performance, model, memory, color):
+        """Инициализатор названия объекта, описания объекта, цены, количества товара в наличии, производительность
         устройства, внутренняя память, цвет устройства"""
-        self.perfomance = perfomance
+        self.performance = performance
         self.model = model
         self.memory = memory
         self.color = color
         super().__init__(name, description, price, quantity_in_stock)
-
-
-
-
-
-
